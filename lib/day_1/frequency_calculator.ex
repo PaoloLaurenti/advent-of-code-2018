439b6baeb884fbc759fef8275b1420ef37fbf688
Paolo Laurenti <laurentipaolo@gmail.com>
2018-12-03 11:08:00 +0100

    add types specs

--- lib/day_1/frequency_calculator.ex
+++ lib/day_1/frequency_calculator.ex
@@ -1,13 +1,16 @@
 defmodule Day1.FrequencyCalculator do
+  @spec calculate(values :: [String.t()]) :: integer()
   def calculate(values) do
     values
     |> Enum.map(&toNumber/1)
     |> Enum.sum()
   end
 
+  @spec toNumber(String.t()) :: integer()
   defp toNumber("+" <> value), do: asInteger(value)
   defp toNumber("-" <> value), do: -asInteger(value)
 
+  @spec asInteger(String.t()) :: integer()
   defp asInteger(value) do
     {res, _} = Integer.parse(value)
     res
