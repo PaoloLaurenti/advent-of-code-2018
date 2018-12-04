663d895961ad0e527aa30f2b7290fe19fe2062ff
Paolo Laurenti <laurentipaolo@gmail.com>
2018-12-04 13:49:32 +0100

    refactoring

--- lib/day_2/checksum_calculator.ex
+++ lib/day_2/checksum_calculator.ex
@@ -1,36 +1,22 @@
 defmodule Day2.ChecksumCalculator do
-  def calculate([_element]) do
-    0
-  end
+  def calculate(input) do
+    appearing_counts =
+      input
+      |> Enum.reduce(%{appearing_twice: 0, appearing_more_than_twice: 0}, fn x, acc ->
+        chars = String.graphemes(x)
+        counts = Enum.reduce(chars, %{}, fn l, acc -> Map.update(acc, l, 1, &(&1 + 1)) end)
 
-  def calculate([first, second]) do
-    counts_first =
-      first
-      |> String.graphemes()
-      |> Enum.reduce(%{}, fn l, acc -> Map.update(acc, l, 1, &(&1 + 1)) end)
+        letters_appearing_twice_count = Enum.count(counts, fn {_, c} -> c == 2 end)
+        letters_appearing_more_than_twice_count = Enum.count(counts, fn {_, c} -> c > 2 end)
 
-    counts_second =
-      second
-      |> String.graphemes()
-      |> Enum.reduce(%{}, fn l, acc -> Map.update(acc, l, 1, &(&1 + 1)) end)
+        acc
+        |> Map.update!(:appearing_twice, &(&1 + letters_appearing_twice_count))
+        |> Map.update!(
+          :appearing_more_than_twice,
+          &(&1 + letters_appearing_more_than_twice_count)
+        )
+      end)
 
-    first_with_letters_appearing_twice =
-      counts_first
-      |> Enum.count(fn {_, c} -> c == 2 end)
-
-    first_with_letters_appearing_more_than_twice =
-      counts_first
-      |> Enum.count(fn {_, c} -> c > 2 end)
-
-    second_with_letters_appearing_twice =
-      counts_second
-      |> Enum.count(fn {_, c} -> c == 2 end)
-
-    second_with_letters_appearing_more_than_twice =
-      counts_second
-      |> Enum.count(fn {_, c} -> c > 2 end)
-
-    (first_with_letters_appearing_twice + first_with_letters_appearing_more_than_twice) *
-      (second_with_letters_appearing_twice + second_with_letters_appearing_more_than_twice)
+    appearing_counts.appearing_twice * appearing_counts.appearing_more_than_twice
   end
 end
