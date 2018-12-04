388549957e1d24f0345ca42e06bf0da2061c6cd8
Paolo Laurenti <laurentipaolo@gmail.com>
2018-12-04 08:48:29 +0100

    day2 - checksum calculator returns zero with one input element with only a letter appearing more than three times

--- test/day_2/checksum_calculator_test.exs
+++ test/day_2/checksum_calculator_test.exs
@@ -18,5 +18,10 @@ defmodule ChecksumyCalculatorTest do
       input = ["abbbcdef"]
       assert ChecksumCalculator.calculate(input) == 0
     end
+
+    test "with only one letter appearing more than three times returns zero as checksum" do
+      input = ["abbbcdbbbef"]
+      assert ChecksumCalculator.calculate(input) == 0
+    end
   end
 end
