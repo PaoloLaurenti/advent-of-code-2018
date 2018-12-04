1917aa248df4565c578d521cc8ab89796fa22edc
Paolo Laurenti <laurentipaolo@gmail.com>
2018-12-04 08:46:56 +0100

    day2 - checksum calculator returns zero with one input element with only a letter appearing exactly twice

--- test/day_2/checksum_calculator_test.exs
+++ test/day_2/checksum_calculator_test.exs
@@ -3,9 +3,14 @@ defmodule ChecksumyCalculatorTest do
 
   alias Day2.ChecksumCalculator
 
-  describe "input with one element with different letters" do
-    test "returns zero as checksum" do
+  describe "input with one element" do
+    test "with different letters returns zero as checksum" do
       input = ["abcdef"]
+      assert ChecksumCalculator.calculate(input) == 0
+    end
+
+    test "with only one letter appearing twice returns zero as checksum" do
+      input = ["abbcdef"]
       assert ChecksumCalculator.calculate(input) == 0
     end
   end
